7c0bd2402509c60a4c51cc5ca9377093f3ee61b1
Devil-Rajpoot-78 <hamza207205@gmail.com>
2026-08-10 14:22:38 +0500

    fix: set React Router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Muhammad-Ameer-Hamza-Portfolio">
       <LoadingScreen />
       <Navigation />
       <Layout>
