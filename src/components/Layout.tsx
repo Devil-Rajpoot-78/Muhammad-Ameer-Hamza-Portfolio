5f72776559f4544785a957a9fbdf2df5629c68ed
Devil-Rajpoot-78 <hamza207205@gmail.com>
2026-08-10 14:29:38 +0500

    fix: remove unused responsive layout state

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,19 +1,9 @@
 import { ReactNode } from 'react';
-import { useWindowSize } from '@hooks/index';
 
-interface LayoutProps {
-  children: ReactNode;
-}
+interface LayoutProps { children: ReactNode; }
 
-export const Layout = ({ children }: LayoutProps) => {
-  const windowSize = useWindowSize();
-  const isMobile = windowSize.width ? windowSize.width < 768 : false;
-
-  return (
-    <div className="min-h-screen bg-black text-white">
-      <div className="relative">
-        {children}
-      </div>
-    </div>
-  );
-};
+export const Layout = ({ children }: LayoutProps) => (
+  <div className="min-h-screen bg-black text-white">
+    <div className="relative">{children}</div>
+  </div>
+);
