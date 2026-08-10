43716b6ae9721ef6e850f0cc8cc6d27cbbc89db2
Devil-Rajpoot-78 <hamza207205@gmail.com>
2026-08-10 14:31:00 +0500

    fix: make Tailwind config compatible with ES modules

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: { black: '#000000', white: '#ffffff' },
+      fontFamily: { sans: ['system-ui', 'sans-serif'] },
+    },
+  },
+  plugins: [],
+};
